1e0bd38a161581d424ad8666fe9500dfc8daa88d
Brendan <brendanyu@protonmail.com>
2019-07-06 15:51:10 -0700

    main creates a JFrame window now, created JFrame window class, created next piece generator code using random number generation, commented out temporary code created for last deliverable

--- src/model/Tetramino.java
+++ src/model/Tetramino.java
@@ -1,5 +1,8 @@
-//package model;
+package model;
 
 //import model.Square;
 
 // not sure how to do this yet
+
+public class Tetramino {
+}
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,18 +1,32 @@
 package ui;
 
-import model.Square;
 //import model.Tetramino;
+//import logic.randNext;
 
 public class Main {
+
+    //public static void printNum() {
+    //    System.out.println(randNext.selectNext());
+    //}
+
     public static void main(String[] args) {
         //Square Blue100 = new Square();
         //Blue100.dimensions = 100;
         //Blue100.colour = "blue";
-        Square Blue100 = new Square(100, "blue");
+        //Square Blue100 = new Square(100, "blue");
 
-        Square.squareDimensions(Blue100);
-        Square.squareColour(Blue100);
+        //Square.squareDimensions(Blue100);
+        //Square.squareColour(Blue100);
 
-        System.out.println("main!");
+        //System.out.println("main!");
+
+        //printNum();
+        //printNum();
+        //printNum();
+        //printNum();
+        //printNum();
+
+        TopLevelWindow.createWindow();
+
     }
 }
