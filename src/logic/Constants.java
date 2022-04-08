f61d1b4ec742aecd9ee73dd9b17f491a056a0a87
iyu46 <ihpkaillus@gmail.com>
2022-04-08 16:11:59 -0700

    update for deliverable 04

---
+++ src/logic/Constants.java
@@ -0,0 +1,14 @@
+package logic;
+
+public class Constants {
+
+    public int gameSpeed;
+
+    //MODIFIES: this
+    //EFFECTS: initializes global constants and default values
+    public void initConstants() {
+        gameSpeed = 1;
+    }
+
+
+}
