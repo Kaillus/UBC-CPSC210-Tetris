06c8360ef4616649bc1e0205d9658699baaa68a9
iyu46 <ihpkaillus@gmail.com>
2022-04-08 16:12:04 -0700

    added meaningless processing of weather condition

--- src/main/ui/ReadWebPageEx.java
+++ src/main/ui/ReadWebPageEx.java
@@ -14,7 +14,7 @@ public class ReadWebPageEx {
 
     private static String getStuff() {
         //String theURL = "https://www.ugrad.cs.ubc.ca/~cs210/2018w1/welcomemsg.html"; //this can point to any URL
-        String weatherQuery = "http://api.openweathermap.org/data/2.5/weather?id=5911606&APPID="; //this can point to any URL
+        String weatherQuery = "http://api.openweathermap.org/data/2.5/weather?id=5911606&APPID=";
         String apiKey = "f509e27e7964150e48e270515be09715";
         return weatherQuery + apiKey;
     }
@@ -51,12 +51,29 @@ public class ReadWebPageEx {
         return json;
     }
 
+    private static String processString(String string) {
+        String weather = string;
+        if (weather.equals("Clouds")) {
+            weather = "Cloudy";
+        } else if (weather.equals("Thunderstorm")) {
+            weather = "Thunderstorming";
+        } else if (weather.equals("Clear")) {
+            //
+        } else if (weather.substring(string.length() - 1).equals("e")) {
+            weather = weather.replaceAll("e$", "y");
+        } else {
+            weather = weather + "y";
+        }
+
+        return weather.toLowerCase();
+    }
+
     public static String fetchString() {
         try {
             JSONObject obj = parseInput(getStuff());
 
             JSONArray arr = obj.getJSONArray("weather");
-            String weather = arr.getJSONObject(0).getString("main");
+            String weather = processString(arr.getJSONObject(0).getString("main"));
 
             String string = "Play Tetris while it's ";
             String string2 = " outside";
