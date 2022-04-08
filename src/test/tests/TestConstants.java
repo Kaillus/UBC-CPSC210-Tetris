2a2d0850ee8df0482204f0b1e534ab553b29179a
iyu46 <ihpkaillus@gmail.com>
2022-04-08 16:12:04 -0700

    add a bunch of tests

---
+++ src/test/tests/TestConstants.java
@@ -0,0 +1,64 @@
+package tests;
+
+import logic.Constants;
+import logic.TetrisProperties;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+public class TestConstants {
+
+    private String testPath = "test.properties";
+    Constants testConstants = new Constants();
+
+    @BeforeEach
+    public void deleteTestFile() {
+        File testFile = new File(testPath);
+        if (testFile.isFile()) {
+            if (testFile.delete()) {
+                System.out.println("Successfully cleared testFile");
+            } else {
+                System.out.println("Failed to delete testFile");
+            }
+        }
+    }
+
+    @Test
+    public void testInit() {
+        File testFile = new File(testPath);
+        testConstants.initConstants(testPath);
+        assertTrue(testFile.isFile());
+        assertEquals(1, testConstants.getGameSpeed());
+    }
+
+    @Test
+    public void testExistingInit() {
+        File testFile = new File(testPath);
+        TetrisProperties.writeToProperties(3,testPath);
+        testConstants.initConstants(testPath);
+        assertTrue(testFile.isFile());
+        assertEquals(3, testConstants.getGameSpeed());
+    }
+
+    public void testGetSet() {
+        File testFile = new File(testPath);
+        testConstants.initConstants(testPath);
+        assertTrue(testFile.isFile());
+        assertEquals(1, testConstants.getGameSpeed());
+        testConstants.setGameSpeed(3);
+        assertEquals(3, testConstants.getGameSpeed());
+    }
+
+    @AfterEach
+    public void deleteTestFile2() {
+        deleteTestFile();
+    }
+
+}
