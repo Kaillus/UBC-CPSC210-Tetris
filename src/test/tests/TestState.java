054c6770f196879cd59d15c02a72417ff9756f06
iyu46 <ihpkaillus@gmail.com>
2022-04-08 16:12:00 -0700

    wip update for deliverable 06

--- src/test/tests/TestState.java
+++ src/test/tests/TestState.java
@@ -2,12 +2,14 @@ package tests;
 
 import logic.State;
 
+import logic.java.UnknownStateException;
 import org.junit.*;
 
 import static junit.framework.TestCase.assertTrue;
 import static logic.State.States.*;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class TestState {
 
@@ -28,5 +30,20 @@ public class TestState {
         assertEquals(state.getState(), PAUSE);
         state.setState(GAME_OVER);
         assertEquals(state.getState(), GAME_OVER);
+    }
+
+    @Test
+    public void testIllegalState() {
+        state.setState(MENU);
+        assertEquals(state.getState(), MENU);
+
+        try{
+            //state.setState("TETRIS");
+        } catch (UnknownStateException e) {
+            fail("Failed to handle UnknownStateException");
+        }
+
+        assertEquals(state.getState(), MENU);
+
     }
 }
